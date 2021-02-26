dbbdddd10f69120be600a6d5c6056be31f0bdadd
Ayub-Hanif <69708555+Ayub-Hanif@users.noreply.github.com>
2021-02-25 21:56:46 -0800

    Update main.cpp

--- starting_SFML/main.cpp
+++ starting_SFML/main.cpp
@@ -1,26 +1,35 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 void Control();
 int main()
 {
+ 
    Control();
-
-
-
-
+  
     return 0;
 }
 void Control()
 {
-    sf::RenderWindow window(sf::VideoMode(1800, 1000), "LoL", sf::Style::Close | sf::Style::Resize);
+    sf::RenderWindow window(sf::VideoMode(1000, 1000), "LoL", sf::Style::Close | sf::Style::Resize);
     sf::RectangleShape player1(sf::Vector2f(100.0f, 100.0f));
-    sf::RectangleShape player2(sf::Vector2f(100.0f, 100.0f));
-    sf::RectangleShape player3(sf::Vector2f(100.0f, 100.0f));
-    player2.setFillColor(sf::Color::Red);
+    sf::RectangleShape line(sf::Vector2f(100.0f,580.0f));
+    
+    // create an array of 3 vertices that define a triangle primitive
+    sf::VertexArray lines(sf::LinesStrip, 5);
+    lines[0].position = sf::Vector2f(900,10);
+    lines[1].position = sf::Vector2f(900, 900);
+    lines[2].position = sf::Vector2f(10, 900);
+    lines[3].position = sf::Vector2f(10, 10);
+    lines[4].position = sf::Vector2f(900, 10);
+    lines[0].color = sf::Color::Red;
+    lines[1].color = sf::Color::Blue;
+    lines[2].color = sf::Color::Yellow;
+    lines[3].color = sf::Color::Green;
+    lines[4].color = sf::Color::White;
+
     player1.setFillColor(sf::Color::Blue);
-    player3.setFillColor(sf::Color::Green);
-    player2.setOrigin(-250.0f, -250.0f);
-    player3.setOrigin(-500.0f, -500.0f);
+
     while (window.isOpen())
     {
         sf::Event evnt;
@@ -31,17 +40,12 @@ void Control()
             case sf::Event::Closed:
                 window.close();
                 break;
-
-            case sf::Event::MouseButtonPressed:
-                player1.move(100.0f, 100.0f);
-                break;
             }
-
         }
+        
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))
         {
             player1.move(-0.5f, 0.0f);
-            exit;
         }
         else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
         {
@@ -57,9 +61,9 @@ void Control()
         }
        
         window.clear();
+        window.draw(lines);
         window.draw(player1);
-        window.draw(player2);
-        window.draw(player3);
+        
         window.display();
     }
 }
